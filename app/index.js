df64134358bd9bcc56bb04fa3838090041bb8619
lvlei <lvlei@missfresh.cn>
2019-11-13 13:06:44 +0800

    fix

--- app/index.js
+++ app/index.js
@@ -19,6 +19,17 @@ mongoose.connect(connectionStr, {
 }, () => console.log('MongoDB 连接成功了！'));
 mongoose.connection.on('error', console.error);
 app.use(koaStatic(path.join(__dirname, 'public')));
+
+app.use(async (ctx, next)=> {
+  ctx.set('Access-Control-Allow-Origin', '*');
+  ctx.set('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+  if (ctx.method == 'OPTIONS') {
+    ctx.body = 200; 
+  } else {
+    await next();
+  }
+});
 app.use(error({
 	postFormat: (e, {
 		stack,
