11f3d38f098284af6fbf2670ccc7905bb23a7a22
lvlei <lvlei@missfresh.cn>
2019-11-13 11:01:36 +0800

    fix

--- app/controllers/server.js
+++ app/controllers/server.js
@@ -2,7 +2,7 @@ const path = require('path');
 class ServerCtl {
     users(ctx) {
         // 返回结果
-     let users = [{id:1,name:'zhufeng12'},{id:2,name:'zhufeng23'},{id:3,name:'zhufeng3'},{id:4,name:'zhufeng6'}];
+     let users = [{id:1,name:'zhufeng12444'},{id:2,name:'zhufeng23'},{id:3,name:'zhufeng3'},{id:4,name:'zhufeng6'}];
     ctx.response.status = 200;
     ctx.body = users;
     }
