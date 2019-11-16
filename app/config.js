2c97f7dbef8062f6aa2f6675cb214a2efd9d38cd
lvlei <lvlei@missfresh.cn>
2019-11-16 18:04:57 +0800

    add

--- app/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    secret: 'zhihu-jwt-secret',
-    connectionStr: 'mongodb+srv://lvlei:896100@zhihu-oay2l.mongodb.net/test?retryWrites=true&w=majority',
-  };
--- app/routers/server.js
+++
@@ -1,7 +0,0 @@
-const Router = require('koa-router')
-const router = new Router()
-
-const { users} = require('../controllers/server')
-router.get('/api/users', users);
-
-module.exports = router
