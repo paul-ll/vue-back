62d163d1e733350e48dff1730c6b663d518d36f5
lvlei <lvlei@missfresh.cn>
2019-11-16 19:31:06 +0800

    add

--- app/routers/category.js
+++
@@ -1,8 +0,0 @@
-const Router = require('koa-router')
-const router = new Router()
-const { categoryList,articleList} = require('../controllers/category')
-
-router.get('/category', categoryList);
-router.get('/article', articleList);
-
-module.exports = router
